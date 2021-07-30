6e7cedd1ecb29de5da776c62a923fd3c84855831
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-07-30 22:59:05 +0900

    2021.07.30. 온라인 체험 scrool.js 추가

---
+++ sub/js/scroll.js
@@ -0,0 +1,15 @@
+const mainNav = document.querySelector(".main_nav");
+
+const handleMenuScroll = (event) => {
+    const scrollTop = event.target.documentElement.scrollTop;
+    if (scrollTop > 1070) {
+        mainNav.style.position = "sticky";
+        mainNav.style.top = "0%";
+        mainNav.style.zIndex = "9999";
+    } else {
+        mainNav.style.position = "relative";
+        mainNav.style.zIndex = "0";
+    }
+}
+
+window.addEventListener("scroll", handleMenuScroll);
